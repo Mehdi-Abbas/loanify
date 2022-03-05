32ac134ec8ffe0d23a0e93cf1e82ef3eec45679a
Mehdi Abbas <mehdiabbas799@gmail.com>
2022-03-06 04:58:01 +0500

    makes installable

---
+++ public/index.js
@@ -0,0 +1,17 @@
+import * as serviceWorker from './worker.js';
+
+//  serviceWorker.register();
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/worker.js', {scope: '/'}).then(function (registration) {
+            console.log('Worker registration successful', registration.scope);
+        }, function (err) {
+            console.log('Worker registration failed', err);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    });
+} else {
+    console.log('Service Worker is not supported by browser.');
+}
